3210de6a8168b14f95d9cc19d61540d82a981a83
Act Sistem <act@Acts-MacBook-Pro.local>
2021-12-11 01:32:39 +0300

    fiber first api

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+type Todo struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Price string `json:"price"`
+}
+
+var todos = []Todo{
+	{ID: 1, Name: "Sarj Aleti", Price: "120,00 TL"},
+	{ID: 2, Name: "Kilif", Price: "75,00 TL"},
+}
+
+func getTodo(c *fiber.Ctx) error {
+	return c.Status(fiber.StatusOK).JSON(todos)
+}
 
 func main() {
+
 	app := fiber.New()
+	app.Use(logger.New())
 
-	app.Get("/:name", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!" + c.Params("name"))
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
 	})
+	app.Get("/api", getTodo)
 
-	app.Listen(":3000")
+	app.Listen(":3001")
 }
